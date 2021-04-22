2d63def46e829522a3035135f0ced64dd511266a
Byeonghoon Jeon <jeon4972@snu.ac.kr>
2021-04-22 13:35:07 +0200

    Add files via upload

---
+++ brand_name_generator.py
@@ -0,0 +1,4 @@
+print("!!Welcome to the Brand Name Genorator!!")
+city = input("Which city did you grow up?\n")
+animal = input("What is your favorite animal?\n")
+print("Your Brand Name is: [" + city, animal + "]")
